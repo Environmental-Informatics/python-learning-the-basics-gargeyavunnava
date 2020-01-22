801eea6ca2deec40b95de90ac63fbea8e30338b2
gargeyavunnava <gargeyavunnava@gmail.com>
2020-01-22 17:17:24 -0500

    Update vvunnava_Exercise_3.2.py

--- vvunnava_Exercise_3.2.py
+++ vvunnava_Exercise_3.2.py
@@ -2,6 +2,7 @@
 Created on Wed Jan 22 11:51:27 2020
 @author: vvunnava
 Assignment_01, Exrecise_3.2, script for part 4 of the question
+program description: Creating functions to run a function twice and print an argument twice
 '''
 
 
